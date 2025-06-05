c464a78743e29f0210361ceee4ca11893e1a185a
Ryan James <realcoder2023@gmail.com>
2025-06-05 19:32:39 -0400

    added a way to user to pick a different rprojecxt type like html css js

--- writeHTMLFromCPP/FeatureLanguagesTemplates.hpp
+++ writeHTMLFromCPP/FeatureLanguagesTemplates.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 
 enum class Languages : int {
@@ -7,3 +8,17 @@ enum class Languages : int {
 	CSS = 2,
 	JS = 3
 };
+
+void createFileFunctionEnable(std::string code, std::ofstream file);
+
+void createFileFunctionEnable(std::string code, std::ofstream file) {
+
+	if (file.is_open()) {
+
+		file << code;
+	}
+
+
+
+	file.close();
+}
